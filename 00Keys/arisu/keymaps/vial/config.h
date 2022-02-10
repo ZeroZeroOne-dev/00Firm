4820564ad40621c75faa17964f2e29fd9c477373
ZeroZeroOne <jeffrey.smets@gmail.com>
2022-02-10 16:24:52 +0100

    arisu

---
+++ 00Keys/arisu/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAE, 0xF8, 0x03, 0x30, 0xB8, 0xB6, 0x88, 0x25}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
